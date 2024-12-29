5b3e380aae61fe1f3e130975e496274dc4bc4fc2
Lee Miller <dleemiller@protonmail.com>
2024-12-29 13:09:31 -0700

    removing dead code

--- src/tokenizer.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENCODZALL_TOKENIZER_H
-#define ENCODZALL_TOKENIZER_H
-
-PyObject* tokenize(PyObject* self, PyObject* args, PyObject *keywds);
-PyObject* make_targets(PyObject* self, PyObject* args, PyObject *keywds);
-
-#endif
-
